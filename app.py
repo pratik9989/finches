2c8b92aeff1984bc6253e4bcd7d5b3f6de2b7e90
Pratik Patel <pratik.patel9989@gmail.com>
2025-07-29 11:44:02 +0200

    Added validation for all fields and type checking, In document observation_id, text_observation written twice, So I believe I have to check all fields.

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Request
+from pydantic import BaseModel, StrictFloat, StrictStr
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from starlette.status import HTTP_400_BAD_REQUEST
 
 app = FastAPI()
 
 class Location(BaseModel):
-    latitude: float
-    longitude: float
+    latitude: StrictFloat
+    longitude: StrictFloat
 
 class Observation(BaseModel):
-    observation_id: str
-    user_id: str
-    text_observation: str
-    photo_url: str
+    observation_id: StrictStr
+    user_id: StrictStr
+    text_observation: StrictStr
+    photo_url: StrictStr
     location: Location
 
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=HTTP_400_BAD_REQUEST,
+        content={
+            "message": "Bad Request. Validation failed.",
+            "errors": exc.errors()
+        },
+    )
 
 @app.post("/observation")
 def submit_observation(observation:Observation):
@@ -21,3 +33,4 @@ def submit_observation(observation:Observation):
         "message": "received",
         "id": observation.observation_id
     }
+
